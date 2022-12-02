f2704c0e9df2dbcff5ad3d2dbedf5c24b3ac7817
CORBEL Marion (SNCF VOYAGEURS / Direction Intercites / EQUIPE 1 ESCALE PAZ) <9301738k@commun.ad.sncf.fr>
2022-12-02 12:26:08 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import questions from "./question";
+import "./App.css";
 
 function App() {
+  const [displayQuestion, setDisplayQuestion] = useState(false);
+  const [displayAnswer, setDisplayAnswer] = useState(false);
+  const [displayStart, setDisplayStart] = useState(true);
+  const [numberOfQuestions, setNumberOfQuestions] = useState(1);
+  const [numberOfGoodAnswers, setNumberOfGoodAnswers] = useState(0);
+  let [x, setX] = useState(Math.floor(Math.random() * 5));
+  console.log(x);
+  const startGame = () => {
+    setDisplayStart(false);
+    setDisplayQuestion(true);
+  };
+
+  const giveAnswer = () => {
+    setDisplayQuestion(false);
+    setDisplayAnswer(true);
+  };
+  const newQuestionGood = () => {
+    setX(Math.floor(Math.random() * 5));
+    setNumberOfQuestions(numberOfQuestions + 1);
+    setDisplayQuestion(true);
+    setDisplayAnswer(false);
+    setNumberOfGoodAnswers(numberOfGoodAnswers+1)
+  };
+  const newQuestionBad = () => {
+    setX(Math.floor(Math.random() * 5));
+    setNumberOfQuestions(numberOfQuestions + 1);
+    setDisplayQuestion(true);
+    setDisplayAnswer(false);
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="revision">
+      {displayStart ? (
+        <div>
+          {" "}
+          <p className="title"> Prêt à révisier pour avoir ton TP?</p>
+          <button
+           onClick={startGame}>C'est parti ! </button>
+        </div>
+      ) : (
+        <div>
+          <h3>Question {numberOfQuestions} </h3>
+        </div>
+      )}
+      {displayQuestion ? (
+        <div>
+          {" "}
+          <p className="title">{questions[x].question}</p>{" "}
+          <button onClick={giveAnswer}>La Réponse</button>{" "}
+        </div>
+      ) : null}
+      {displayAnswer ? (
+        <div>
+          {" "}
+          <p className="title">{questions[x].answer}</p>{" "}
+          <button onClick={newQuestionGood}>Bonne réponse</button>{" "}
+          <button onClick={newQuestionBad}>Mauvaise réponse</button>{" "}
+        </div>
+      ) : null}
+      {displayStart? null : <p>Score : {numberOfGoodAnswers} / {numberOfQuestions-1}</p>}
     </div>
   );
 }
